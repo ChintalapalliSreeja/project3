ca9d122fcbc9414a7682d4c7f4ee000343e109d8
Chintalapalli sreeja <chsreeja2710@gmail.com>
2025-08-21 20:08:58 +0530

    modified files from stash

--- file1.java
+++ file1.java
@@ -1,1 +1,10 @@
+<<<<<<< Updated upstream
 Syatem.out.println("Hello Everyone");
+=======
+class public{
+    public static void main(String[] args){
+        Scanner s = s.Scanner
+        Syatem.out.println("Java files in github")
+    } 
+}
+>>>>>>> Stashed changes
